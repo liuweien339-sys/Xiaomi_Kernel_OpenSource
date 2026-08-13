62e4798a8aa4b25bd5b7694c29e3e72a0288ec6d
普通的玩家863 <liuweien339@gmail.com>
2026-08-13 15:25:51 +0800

    Update mtk_drm_mmp.c

--- drivers/gpu/drm/mediatek/mtk_drm_mmp.c
+++ drivers/gpu/drm/mediatek/mtk_drm_mmp.c
@@ -277,7 +277,7 @@ struct CRTC_MMP_Events *get_crtc_mmp_events(unsigned long id)
 #ifdef CONFIG_MTK_IOMMU_V2
 #include <mtk_iommu_ext.h>
 #endif
-#include <mtk_drm_drv.h>
+#include "mtk_drm_drv.h"
 
 #define DISP_PAGE_MASK 0xfffL
 
