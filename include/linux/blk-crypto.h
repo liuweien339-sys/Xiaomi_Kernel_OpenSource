721a83b425f5f52eed24e61d1ba31f08a2654415
普通的玩家863 <liuweien339@gmail.com>
2026-08-13 18:14:33 +0800

    Update blk-crypto.h

--- include/linux/blk-crypto.h
+++ include/linux/blk-crypto.h
@@ -32,9 +32,6 @@ int blk_crypto_start_using_mode(enum blk_crypto_mode_num crypto_mode,
 int blk_crypto_evict_key(struct request_queue *q,
 			 const struct blk_crypto_key *key);
 
-static inline void blk_crypto_flock(struct keyslot_manager *ksm, 
-                unsigned int flags) {}
-
 #else /* CONFIG_BLK_INLINE_ENCRYPTION */
 
 static inline int blk_crypto_submit_bio(struct bio **bio_ptr)
